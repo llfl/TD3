7a6b3470940c4818cd735eae8eda6c4674061139
Kun <quinnHugh@hotmail.com>
2019-09-07 15:42:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ def evaluate_policy(policy, eval_episodes=10):
     for _ in range(eval_episodes):
         obs = env.reset()
         done = False
-        print(np.array(list(obs),dtype=np.float32))
+        print(np.array(list(obs)))
         while not done:
-            action = policy.select_action(np.array(list(obs.values()),dtype=np.float32))
+            action = policy.select_action(np.array(list(obs)))
             obs, reward, done, _ = env.step(action)
             avg_reward += reward
 
